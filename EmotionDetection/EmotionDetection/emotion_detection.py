16e9d36664d7560dbcc550a9bad119d28ce59bf9
AnNguyenf <huongpnxa@gmail.com>
2026-03-20 00:21:57 +0700

    Create emotion_detection.py

---
+++ EmotionDetection/EmotionDetection/emotion_detection.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    
+    response = requests.post(url, json = myobj, headers = header)
+    
+    # Chuyển đổi chuỗi phản hồi thành dictionary (JSON)
+    formatted_response = json.loads(response.text)
+    
+    # Trích xuất các giá trị cảm xúc (nằm trong emotionPredictions[0])
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    
+    # Tìm cảm xúc có điểm số cao nhất (Dominant Emotion)
+    emotion_list = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_list, key=emotion_list.get)
+    
+    # Trình bày kết quả theo định dạng yêu cầu
+    result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    
+    return result
